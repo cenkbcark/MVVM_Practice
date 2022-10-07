e4abdf28c7c2aa9bac9d2a1258a69af6d2053e42
cenkbcark <79763515+cenkbcark@users.noreply.github.com>
2022-10-07 23:43:48 +0300

    Network layer has been written.

--- MVVM_Practice/ViewController.swift
+++ MVVM_Practice/ViewController.swift
@@ -12,6 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let networkManager = NetworkManager(session: .shared)
+        
+        AlbumAPI(networkManager: networkManager).retriveAlbum(request: .init(userId: 1)) { result in
+            switch result {
+            case .success(let response):
+                print("fooo response: \(response)")
+            case .failure(let error):
+                print("fooo error: \(error.description)")
+            }
+        }
+        
+        UserAPI(networkManager: networkManager).retriveUser(request: .init()) { result in
+            switch result {
+            case .success(let success):
+                print("fooooo user: \(success)")
+            case .failure(let error):
+                print("fooooo error: \(error)")
+            }
+        }
+        
+        PhotosAPI(networkManager: networkManager).retrieveAlbum(request: .init(albumId: 1)) { result in
+            switch result {
+            case .success(let success):
+                print("fooo photos: \(success)")
+            case .failure(let error):
+                print("fooo error: \(error)")
+            }
+        }
     }
 
 
